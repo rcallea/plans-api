80ec9fcdb25e5c19f612434cd5a974223c76ef0e
Edgar Salamanca <edgar.ariel.salamanca@gmail.com>
2023-10-29 07:24:51 -0500

    cambios en el coverage

--- src/__init__.py
+++ src/__init__.py
@@ -4,7 +4,8 @@ from flask import Flask
 def create_app(config_name):
     app = Flask(__name__)
     app.config[
-        'SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@database-sportsapp.cg2ezd48zwyn.us-east-2.rds.amazonaws.com:5432/sportmanins'
+        #'SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@database-sportsapp.cg2ezd48zwyn.us-east-2.rds.amazonaws.com:5432/sportmanins'
+        'SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/postgres'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"pool_pre_ping": True, "pool_recycle": 600}
     return app
