404bfd38ab268f721ebd3c0308a52037833dc424
Edgar Salamanca <edgar.ariel.salamanca@gmail.com>
2023-11-05 14:10:08 -0500

    cambios para validar el quality gate y el coverage

--- src/__init__.py
+++ src/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 def create_app(config_name):
     app = Flask(__name__)
     app.config[
-        'SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@dbmetricas.cabyk5komby1.us-east-1.rds.amazonaws.com:5432/planes'
+        'SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@databasemetricas.cabyk5komby1.us-east-1.rds.amazonaws.com:5432/planes2'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"pool_pre_ping": True, "pool_recycle": 600}
     return app
